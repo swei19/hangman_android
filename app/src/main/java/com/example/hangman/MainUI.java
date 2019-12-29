b99a0e30a8c3408ca5753b1a005fa7fc4e64dff7
Spencer <swei19@seas.upenn.edu>
2019-12-28 19:46:03 -0800

    Hint System Implemented

--- app/src/main/java/com/example/hangman/MainUI.java
+++ app/src/main/java/com/example/hangman/MainUI.java
@@ -14,15 +14,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class MainUI extends AppCompatActivity {
 
     private final static int NUMBER_OF_WRONG_GUESSES_ALLOWED = 6;
+    private final static int NUMBER_OF_HINTS_ALLOWED = 2;
     private Score score;
     private int currentWrongGuesses = 0;
     private int currentCorrectGuesses = 0;
     private int competitiveCarryOverScore = 0;
+    private int currentNumHintUsed = 0;
 
     private ArrayList<String> guessedLetters = new ArrayList<String>();
     private Display currentDisplay;
@@ -147,13 +150,30 @@ public class MainUI extends AppCompatActivity {
 
     public void onClick(View v) {
         TextView toGuess = findViewById(R.id.guessWordAndUnderlines);
+
         if (currentWrongGuesses < NUMBER_OF_WRONG_GUESSES_ALLOWED){
             Button currentButton = findViewById(v.getId());
-            guessedLetters.add((String) currentButton.getText());
+            CharSequence letterToAdd = currentButton.getText();
+            if (currentButton.getText().equals("Hint")){
+                System.out.println("HELLO MY FRIEND");
+                Random rand = new Random();
+                int chooseRand  = rand.nextInt(currentGuessWord.length());
+                letterToAdd = currentGuessWord.substring(chooseRand, chooseRand + 1);
 
+                while (guessedLetters.contains(letterToAdd)){
+                    chooseRand  = rand.nextInt(currentGuessWord.length());
+                    letterToAdd = currentGuessWord.substring(chooseRand, chooseRand + 1);
+                }
+
+                System.out.println("HELLO MY FRIEND:" + letterToAdd);
+                currentNumHintUsed += 1;
+            }
+            guessedLetters.add((String) letterToAdd);
+            TextView debug = findViewById(R.id.debugText);
+            debug.setText(letterToAdd);
             toGuess.setText(currentDisplay.displayLetterAndEmptyWordUnderlines(guessedLetters));
 
-            if (uniqueLettersOfGuessedWord.contains(currentButton.getText())) {
+            if (uniqueLettersOfGuessedWord.contains(letterToAdd)) {
                 currentCorrectGuesses += 1;
 
                 if (gameIsWon(uniqueLettersOfGuessedWord)) {
@@ -180,8 +200,16 @@ public class MainUI extends AppCompatActivity {
 
             }
 
+            if (currentButton.getText().equals("Hint")) {
+                if (currentNumHintUsed == NUMBER_OF_HINTS_ALLOWED){
+                    currentButton.setClickable(false);
+                }
 
-            currentButton.setClickable(false);
+            } else {
+                currentButton.setClickable(false);
+            }
+
+
         } else {
             toGuess.setText(currentGuessWord);
         }
@@ -190,6 +218,7 @@ public class MainUI extends AppCompatActivity {
 
 
     }
+
 }
 
 
